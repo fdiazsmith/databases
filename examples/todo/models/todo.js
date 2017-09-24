8a69338d3425e9bcac872672308528017905f68b
Fernando Diaz Smith <fdiazsmith@gmail.com>
2017-09-24 15:55:25 +0200

    put queries back

--- examples/todo/models/todo.js
+++ examples/todo/models/todo.js
@@ -1,37 +1,37 @@
-const fs = require('fs')
+const FS = require('fs')
 const Path = require('path')
 const uuid = require('uuid/v4')
-const config = require('./config-secret.json')
+
+const filename = Path.resolve(__dirname, '../data/todos.json')
 
 
 var mysql      = require('mysql')
 
 var connection = mysql.createConnection({
-  host     : config.host,
-  user     : config.user,
-  password : config.password,
-  database : config.database
+  host     : 'localhost',
+  user     : 'fer',
+  password : 'fer',
+  database : 'hyf_todo'
 })
 
 class Todo {
 
   //NOTE: future implementation, add user ID here.
   init(){
-    console.log(config);
     //Connect to mysql
     connection.connect()
     // define which database to use
-    connection.query('SOME * SQL STATEMENT ? HERE',  (error, results, fields) =>{
+    connection.query('USE hyf_todo',  (error, results, fields) =>{
       if (error) throw error
     })
 
     // check for tables and create if it is not there.
-    connection.query("SOME * SQL STATEMENT ? HERE", (error, results, fields)=>{
+    connection.query("SHOW TABLES LIKE 'task'", (error, results, fields)=>{
       if (error) throw error
 
       if(results.length === 0){
         console.log("Creating TABLE 'task'")
-        connection.query('SOME * SQL STATEMENT ? HERE', (error, results, fields) => {
+        connection.query('CREATE TABLE task (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, description VARCHAR(50), done BOOL, date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,  date_last_modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP);', (error, results, fields) => {
           if (error) throw error
           console.log('Table created: ', results)
         })
@@ -43,7 +43,7 @@ class Todo {
   }
 
   load(callback) {
-    let sql ='SOME * SQL STATEMENT ? HERE'
+    let sql ='SELECT * FROM `task`'
     connection.query(sql, (error, results, fields) => {
       if ( error ) throw error
       callback(error, results)
@@ -52,7 +52,7 @@ class Todo {
 
 
   create(_description, callback) {
-    let sql = 'SOME * SQL STATEMENT ? HERE'
+    let sql = 'INSERT INTO `task` (`id`,`description`,`done`,`date_created`,`date_last_modified`) VALUES (NULL, ? , "0", NULL, NULL)'
     let inserts = [_description];
     sql = mysql.format(sql, inserts);
 
@@ -63,7 +63,8 @@ class Todo {
   }
 
   update(id, description, callback) {
-    let sql = 'SOME * SQL STATEMENT ? HERE'
+    let sql = 'UPDATE `task` SET description = ? WHERE id=?'
+    console.log("id ", id);
     let inserts = [description, parseInt(id.split(":")[1])]
     sql = mysql.format(sql, inserts)
 
@@ -81,7 +82,7 @@ class Todo {
   }
 
   remove(id, callback) {
-    let sql = 'SOME * SQL STATEMENT ? HERE'
+    let sql = 'DELETE FROM `task`  WHERE id=?'
     let inserts = [parseInt(id.split(":")[1])]
     sql = mysql.format(sql, inserts)
 
