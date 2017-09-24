0a6d110533d4f20021b8f642c371aa03e6ea26ae
Fernando Diaz Smith <fdiazsmith@gmail.com>
2017-09-24 14:36:53 +0200

    basic example and connection

--- examples/basic_mysql_connection/index.js
+++ examples/basic_mysql_connection/index.js
@@ -1,20 +1,21 @@
-var fs = require('fs');
-var mysql = require('mysql');
+var fs = require('fs')
+var mysql = require('mysql')
 
 var config = JSON.parse(fs.readFileSync("config-secret.json"))
+console.log( config )
 
 var connection = mysql.createConnection({
-	host: config.host,
-	user: config.user,
-	password: config.password,
-	port: config.port,
-    database: config.database
-});
+  host     : config.host,
+  user     : config.user,
+  password : config.password,
+  database : config.database
+})
 
-connection.connect();
+connection.connect( )
 
-connection.query('SELECT * FROM todos', function (error, results, fields) {
-    console.log(results);
-});
+connection.query('SELECT 2 + 2 AS result', function (error, results, fields) {
+		if ( error ) console.error( error )
+		else console.log( results )
+})
 
-connection.end();
+connection.end( )
