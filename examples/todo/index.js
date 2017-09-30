4bd131f4d1a1b225452538d2da4a1da86f174026
Fernando Diaz Smith <fdiazsmith@gmail.com>
2017-09-30 18:30:24 +0200

    update routes

--- examples/todo/index.js
+++ examples/todo/index.js
@@ -8,42 +8,49 @@ const app = Express()
 const __dirname = '/Users/fdiazsmith/Documents/HYF/databases/examples/todo/'
 
 
-app.use(passport.initialize());
+app.use(passport.initialize())
 
 // parse application/x-www-form-urlencoded
 // for easier testing with Postman or plain HTML forms
 app.use(bodyParser.urlencoded({
   extended: true
-}));
+}))
 
 // parse application/json
 app.use(bodyParser.json())
 
-app.use(Express.static('./public'));
+app.use(Express.static('./public'))
 
-const {list, create, update, remove, authenticate } = require('./actions')
+const {list, create, update, remove, authenticate, signin} = require('./actions')
 
 app.get('/', (req, res)=>{
-  res.sendFile(__dirname + 'public/views/index.html');
+  res.sendFile(__dirname + 'public/views/index.html')
   // NOTE: implement path.join at a later time
   // res.sendFile(path.join(__dirname, '../public', 'index1.html'));
 })
 
-app.get('/todos', list)
+app.get('/todos', passport.authenticate('jwt', { session: false }), list)
+
 app.post('/todos', create)
+
 app.put('/todos/:id', update)
+
 app.delete('/todos/:id', remove)
 
 app.post("/login", authenticate);
 
-app.get("/secret", passport.authenticate('jwt', { session: true }), function(req, res){
-  res.json({message: "Success! You can not see this without a token"});
-});
+app.get("/signin", (req, res)=>{
+  res.sendFile(__dirname + 'public/views/signin.html')
+})
+app.post("/signin", signin)
 
-
-app.listen(3000, ()=>{
-  console.info("SERVER IS UP");
+app.get("/secret", passport.authenticate('jwt', { session: false }), function(req, res){
+  res.json({message: "Success! You can not see this without a token"})
 })
 
+
+app.listen(3000, ()=>{console.info("SERVER IS UP")})
+
 Todo.init()
+
 Users.init()
