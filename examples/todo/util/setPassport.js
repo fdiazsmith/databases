a3744cae25bfa83fdce9c7898c03ada87fb40d34
Fernando Diaz Smith <fdiazsmith@gmail.com>
2017-09-30 19:12:21 +0200

    update strategy to use myslq

--- examples/todo/util/setPassport.js
+++ examples/todo/util/setPassport.js
@@ -1,12 +1,12 @@
 const bodyParser = require('body-parser')
-const _ = require("lodash");
 const jwt = require('jsonwebtoken');
 const passport = require("passport");
 const passportJWT = require("passport-jwt");
 const ExtractJwt = passportJWT.ExtractJwt;
 const JwtStrategy = passportJWT.Strategy;
 const config = require('./passport-secret.json')
-
+const Users = require('../models/users')
+const bcrypt = require('bcrypt')
 
 // SET UP JWT
 let jwtOptions = {}
@@ -16,16 +16,20 @@ jwtOptions.secretOrKey = config.secretOrKey;
 let strategy = new JwtStrategy(jwtOptions, function(jwt_payload, next) {
   console.log('payload received', jwt_payload);
   // usually this would be a database call:
-  let user = users[_.findIndex(users, {id: jwt_payload.id})];
-  if (user) {
-    next(null, user);
-  } else {
-    next(null, false);
-  }
+
+  Users.findOneByID( jwt_payload.id, (error, results )=>{
+    if ( error ) throw error
+    if(results[0] === undefined){
+      next(null, false);
+    }
+    else if( results[0].id ){
+      next(null, results[0] );
+    }
+  })
+
 });
 
 // S E T   U P   U S A G E
-
 passport.use(strategy);
 
 module.exports = {
